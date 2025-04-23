eb3ce5979591e6b3a7437a76841a3bec77c1d7af
Gedeon M. LEKOUNDA <gedeon.lekounda@gmail.com>
2025-04-23 18:12:41 +0100

    decompose dashboard

---
+++ src/views/dashboard/TabList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="card-body">
+      <p class="card-title">Recent Purchases</p>
+      <div class="table-responsive">
+        <table id="recent-purchases-listing" class="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Status report</th>
+              <th>Office</th>
+              <th>Price</th>
+              <th>Date</th>
+              <th>Gross amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(purchase, index) in purchases" :key="index">
+              <td>{{ purchase.name }}</td>
+              <td>{{ purchase.status }}</td>
+              <td>{{ purchase.office }}</td>
+              <td>{{ purchase.price }}</td>
+              <td>{{ purchase.date }}</td>
+              <td>{{ purchase.grossAmount }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  defineProps({
+    purchases: Array, // Liste des achats récents
+  })
+  </script>
+  
