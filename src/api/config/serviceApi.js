bc48cfd0dc1f930d4f824fc1445c7f1a74c82e1d
Gedeon M. LEKOUNDA <gedeon.lekounda@gmail.com>
2025-07-14 21:21:04 +0100

    review serviceApi.js

--- src/api/config/serviceApi.js
+++ src/api/config/serviceApi.js
@@ -1,4 +1,18 @@
 import { useErrorStore } from '@/stores/messages/errorStore';
+import { useMessageStore } from '@/stores/messages/messageStore';
+
+// Fonction de traitement des erreurs
+function handleApiError(error) {
+  // Stocker dans Pinia
+  try {
+    const errorStore = useErrorStore();
+    errorStore.addError(error);
+  } catch (e) {
+    console.warn('Erreur non traitée dans errorStore', error);
+  }
+
+  throw error; // Re-propagation pour traitement dans le composant si nécessaire
+}
 
 const buildService = (client) => ({
   get: async (url, params = {}) => {
@@ -6,38 +20,58 @@ const buildService = (client) => ({
       const response = await client.get(url, { params });
       return response.data;
     } catch (error) {
-      useErrorStore().addError(error);
-      throw error;
+      handleApiError(error);
     }
   },
 
-  post: async (url, data) => {
+  post: async (url, data, showMessage = true) => {
     try {
       const response = await client.post(url, data);
+
+      // Message de confirmation automatique (optionnel)
+      if (showMessage) {
+        try {
+          const messageStore = useMessageStore();
+          messageStore.addMessage('Création réussie');
+        } catch (e) {
+          console.warn('Erreur dans messageStore');
+        }
+      }
       return response.data;
     } catch (error) {
-      useErrorStore().addError(error);
-      throw error;
+      handleApiError(error);
     }
   },
 
-  put: async (url, data) => {
+  put: async (url, data, showMessage = true) => {
     try {
       const response = await client.put(url, data);
+
+      if (showMessage) {
+        try {
+          const messageStore = useMessageStore();
+          messageStore.addMessage('Mise à jour réussie');
+        } catch (e) {}
+      }
+
       return response.data;
     } catch (error) {
-      useErrorStore().addError(error);
-      throw error;
+      handleApiError(error);
     }
   },
 
-  delete: async (url) => {
+  delete: async (url, showMessage = true) => {
     try {
       const response = await client.delete(url);
+      if (showMessage) {
+        try {
+          const messageStore = useMessageStore();
+          messageStore.addMessage('Suppression réussie');
+        } catch (e) {}
+      }
       return response.data;
     } catch (error) {
-      useErrorStore().addError(error);
-      throw error;
+      handleApiError(error);
     }
   },
 });
