733bce92cca58392a8b95c70e463fac3cb75742b
Gedeon M. LEKOUNDA <gedeon.lekounda@gmail.com>
2025-08-07 10:35:51 +0100

    update listconcours pagination

--- src/views/concours/editions/components/table/ListConcour.vue
+++ src/views/concours/editions/components/table/ListConcour.vue
@@ -4,17 +4,17 @@
       <thead>
         <tr>
           <th>#</th>
-          <th>Désignation</th>
-          <th>Type de Concours</th>
-          <th>Date de Début</th>
-          <th>Date de Fin</th>
-          <th>Statut</th>
+          <th>désignation</th>
+          <th>type concours</th>
+          <th>date de début</th>
+          <th>date de fin</th>
+          <th>statut</th>
           <th></th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(concour, index) in concours" :key="concour.id">
-          <td>{{ index + 1 }}</td>
+        <tr v-for="(concour, index) in paginatedConcours" :key="concour.id">
+          <td>{{ (currentPage - 1) * itemsPerPage + index + 1 }}</td>
           <td>{{ concour.designation }}</td>
           <td>{{ concour.type_code }}</td>
           <td>{{ formatDate(concour.date_debut) }}</td>
@@ -40,18 +40,33 @@
         </tr>
       </tbody>
     </table>
+    <Pagination
+      v-model="currentPage"
+      :items-per-page="itemsPerPage"
+      :total-items="concours.length"
+      @update:itemsPerPage="itemsPerPage = $event"
+    />
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import dayjs from 'dayjs'; // ici c'est pour gerer les formats des dates
 import { getConcours } from '@/api/gestions/gestionApi';
 //Ici j'appelle la fonction getconcours qui se trouve dans /api/gestion/gestionApi.js
 //pour recuper tout les concours
 
 import ItemActions from '../details/ItemActions.vue';
+import Pagination from '@/components/shared/Pagination.vue';
+
 const concours = ref([]); //une variable, un table pour recevoir tout les concours
+const currentPage = ref(1);
+const itemsPerPage = ref(10);
+const paginatedConcours = computed(() => {
+  const start = (currentPage.value - 1) * itemsPerPage.value;
+  const end = start + itemsPerPage.value;
+  return concours.value.slice(start, end);
+});
 
 //Une fois les données recuper l'afficher sur la page, on fait ce script et partout ce sera comme ca.
 onMounted(async () => {
